a613c80a03f4884b863c70629a6bae5dcf2fe0b3
aashishsood <aashish.sood@outlook.com>
2026-02-13 00:20:04 +0000

    trips assets

---
+++ dagster_university/dagster_essentials/src/dagster_essentials/defs/assets/trips.py
@@ -0,0 +1,29 @@
+import requests
+from dagster_essentials.defs.assets import constants
+import dagster as dg
+
+@dg.asset
+# src/dagster_essentials/defs/assets/trips.py
+def taxi_trips_file() -> None:
+    """
+      The raw parquet files for the taxi trips dataset. Sourced from the NYC Open Data portal.
+    """
+    month_to_fetch = '2023-03'
+    raw_trips = requests.get(
+        f"https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_{month_to_fetch}.parquet"
+    )
+
+    with open(constants.TAXI_TRIPS_TEMPLATE_FILE_PATH.format(month_to_fetch), "wb") as output_file:
+        output_file.write(raw_trips.content)
+
+
+@dg.asset
+def taxi_zones_file() -> None:
+    """
+      This asset will contain a unique identifier and name for each part of NYC as a distinct taxi zone.
+    """
+    nyc_taxi_zones = requests.get(
+        "https://community-engineering-artifacts.s3.us-west-2.amazonaws.com/dagster-university/data/taxi_zones.csv"
+    )
+    with open(constants.TAXI_ZONES_FILE_PATH, "wb") as output_file:
+        output_file.write(nyc_taxi_zones.content)
